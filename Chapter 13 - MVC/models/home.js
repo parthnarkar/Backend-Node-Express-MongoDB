374e71a9e8ac64a728d1c6725d3cd73867bc6884
Parth Narkar <parthnarkarofficial@gmail.com>
2026-02-03 01:23:14 +0530

    feat: Implement home data management with JSON storage and update fetching logic

--- Chapter 13 - MVC/models/home.js
+++ Chapter 13 - MVC/models/home.js
@@ -1,5 +1,7 @@
-//fake database
-const registeredHomes = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/pathUtil');
+const { error } = require('console');
 
 module.exports = class Home {
   constructor(houseName, housePrice, houseLoc, houseRating, houseImage) {
@@ -11,11 +13,20 @@ module.exports = class Home {
   }
 
   save() {
-    registeredHomes.push(this);
+    Home.fetchAll(registeredHomes => {
+      registeredHomes.push(this);
+      const homeDataPath = path.join(rootDir, 'data', 'homes.json');
+      fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), error => {
+        console.log(error);
+      });
+    })
   }
 
-  static fetchAll() {
-    return registeredHomes;
+  static fetchAll(callback) {
+    const homeDataPath = path.join(rootDir, 'data', 'homes.json');
+    fs.readFile(homeDataPath, (err, data) => {
+      callback(!err ? JSON.parse(data) : []);
+    });
   }
 
 }
